7f91ae68596919e66ad24a6fc7bd0c41b2987295
farahzuot <farahz997@eng.hu.edu.jo>
2020-12-05 19:22:42 +0200

    finished

--- tests/data_structures/test_linked_list.py
+++ tests/data_structures/test_linked_list.py
@@ -1,8 +1,24 @@
-from data_structures_and_algorithms.data_structures.linked_list.linked_list import (
-    LinkedList,
-)
 
+from data_structures_and_algorithms.data_structures.linked_list.linked_list import Linked_list
 
 def test_instance():
-    ll = LinkedList()
-    assert isinstance(ll, LinkedList)
+    ll = Linked_list()
+    assert isinstance(ll, Linked_list)
+
+def test_empty_linked_list():
+    acutal = Linked_list()
+    assert acutal.head == None
+
+def test_insert():
+    a = Linked_list()
+    a.insert('Hello')
+    a.insert('world')
+    b = a.includes('world')
+    assert a.__str__() == '{world}->{Hello}->NULL'
+
+
+def test_includes():
+    a = Linked_list()
+    a.insert('Hello')
+    a.insert('world')
+    assert a.includes('world')
