247c616e69396b83edd10afd2f852aa9bf56450e
farahzuot <farahz997@eng.hu.edu.jo>
2020-12-19 23:21:39 +0200

    trees

---
+++ tests/data_structures/test_trees.py
@@ -0,0 +1,56 @@
+from data_structures_and_algorithms.challenges.tree.tree import BinarySearchTree, BinaryTree, Node
+
+def test_empty_tree():
+    '''
+    Can successfully instantiate an empty tree
+    '''
+    bt = BinaryTree()
+    assert bt.preOrder() == []
+
+def test_single_tree():
+    '''
+    Can successfully instantiate a tree with a single root node
+    '''
+    bt = BinaryTree()
+    bt.root = Node(2)
+    assert bt.preOrder() == [2]
+
+def test_single_tree_left_right():
+    '''
+    Can successfully add a left child and right child to a single root node
+    '''
+    bt = BinaryTree()
+    bt.root = Node(2)
+    bt.root.left=Node(3)
+    bt.root.right=Node(4)
+    assert bt.preOrder() == [2,3,4]
+
+def test_single_tree_preorder():
+    '''
+    Can successfully return a collection from a preorder traversal
+    '''
+    bt = BinaryTree()
+    bt.root = Node(2)
+    bt.root.left=Node(3)
+    bt.root.right=Node(4)
+    assert bt.preOrder() == [2,3,4]
+
+def test_single_tree_inorder():
+    '''
+    Can successfully return a collection from an inorder traversal
+    '''
+    bt = BinaryTree()
+    bt.root = Node(2)
+    bt.root.left=Node(3)
+    bt.root.right=Node(4)
+    assert bt.inOrder() == [3,2,4]
+
+def test_single_tree_postorder():
+    '''
+    Can successfully return a collection from a postorder traversal
+    '''
+    bt = BinaryTree()
+    bt.root = Node(2)
+    bt.root.left=Node(3)
+    bt.root.right=Node(4)
+    assert bt.postOrder() == [3,4,2]
